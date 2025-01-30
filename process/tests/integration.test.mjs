014181d79133e417b99fafc9eb0a371173a03b7d
Ariel Melendez <ariel@ardrive.io>
2025-01-30 12:14:21 -0800

    test(proposals): some test consolidation PE-7382

--- process/tests/integration.test.mjs
+++ process/tests/integration.test.mjs
@@ -17,6 +17,7 @@ describe('AOS Handlers:', () => {
   describe("Propose-Add-Controller", () => {
     let testMemory;
     before(async () => {
+      // Establishes the PROCESS_OWNER as the first controller by way of lazy initialization
       const result = await handle({
         options: {
           Tags: [
@@ -27,166 +28,138 @@ describe('AOS Handlers:', () => {
       testMemory = result.Memory;
     });
 
-    it('should not allow submitting the proposal from a non-controller', async () => {
-      const result = await handle({
-        options: {
-          Tags: [
-            { name: 'Action', value: 'Propose-Add-Controller' },
-            { name: 'Controller', value: 'non-controller' },
-          ],
-          From: "non-controller",
-          Owner: "non-controller",
-        },
-        mem: testMemory,
-      });
+    const invalidProposalTest = async (options, expectedError) => {
+      const result = await handle({ options, mem: testMemory });
       assert.equal(result.Messages?.length, 1, "Expected one message");
       const replyMessage = result.Messages[0];
       const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
       assert.notEqual(actionTag, undefined, "Expected an action tag");
       assert.equal(actionTag.value, 'Invalid-Propose-Add-Controller-Notice');
       const errorTag = replyMessage.Tags.find(tag => tag.name === 'Error');
-      assert(errorTag.value.includes('Sender is not a registered Controller!'));
-      assert(replyMessage.Data.includes('Sender is not a registered Controller!'));
+      assert(errorTag.value.includes(expectedError));
+      assert(replyMessage.Data.includes(expectedError));
+    };
+
+    it('should not allow submitting the proposal from a non-controller', async () => {
+      await invalidProposalTest({
+        Tags: [
+          { name: 'Action', value: 'Propose-Add-Controller' },
+          { name: 'Controller', value: 'non-controller' },
+        ],
+        From: "non-controller",
+        Owner: "non-controller",
+      }, 'Sender is not a registered Controller!');
     });
 
     it('should not allow proposing an existing controller', async () => {
-      const result = await handle({
-        options: {
-          Tags: [
-            { name: 'Action', value: 'Propose-Add-Controller' },
-            { name: 'Controller', value: PROCESS_OWNER },
-          ],
-        },
-        mem: testMemory,
-      });
-      assert.equal(result.Messages?.length, 1, "Expected one message");
-      const replyMessage = result.Messages[0];
-      const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
-      assert.notEqual(actionTag, undefined, "Expected an action tag");
-      assert.equal(actionTag.value, 'Invalid-Propose-Add-Controller-Notice');
-      const errorTag = replyMessage.Tags.find(tag => tag.name === 'Error');
-      assert(errorTag.value.includes('Controller already exists'));
-      assert(replyMessage.Data.includes('Controller already exists'));
+      await invalidProposalTest({
+        Tags: [
+          { name: 'Action', value: 'Propose-Add-Controller' },
+          { name: 'Controller', value: PROCESS_OWNER },
+        ],
+        }, 'Controller already exists');
     });
 
     it('should require a Controller tag', async () => {
-      const result = await handle({
-        options: {
-          Tags: [
-            { name: 'Action', value: 'Propose-Add-Controller' },
-          ],
-        },
-        mem: testMemory,
-      });
+      await invalidProposalTest({
+        Tags: [
+          { name: 'Action', value: 'Propose-Add-Controller' },
+        ],
+        }, 'Controller is required');
+    });
+
+    const validProposalTest = async (options, expectedProposalName, expectedProposal, expectProposalEnd = true) => {
+      const result = await handle({ options, mem: testMemory });
       const replyMessage = result.Messages[0];
       const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
       assert.notEqual(actionTag, undefined, "Expected an action tag");
-      assert.equal(actionTag.value, 'Invalid-Propose-Add-Controller-Notice');
-      const errorTag = replyMessage.Tags.find(tag => tag.name === 'Error');
-      assert(errorTag.value.includes('Controller is required'));
-      assert(replyMessage.Data.includes('Controller is required'));
-    });
+      assert.equal(actionTag.value, 'Propose-Add-Controller-Notice');
+      assert.deepEqual(JSON.parse(replyMessage.Data), { proposalName: expectedProposalName, ...expectedProposal});
+      const proposals = await getProposals(result.Memory);
+      assert.deepEqual(proposals, expectProposalEnd ? {} : { [expectedProposalName]: expectedProposal });
+      return result.Memory;
+    };
 
     it('should successfully create a proposal', async () => {
-      const result = await handle({
-        options: {
+      await validProposalTest(
+        {
           Tags: [
             { name: 'Action', value: 'Propose-Add-Controller' },
             { name: 'Controller', value: 'new-controller' },
           ],
         },
-        mem: testMemory,
-      });
-      const replyMessage = result.Messages[0];
-      const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
-      assert.notEqual(actionTag, undefined, "Expected an action tag");
-      assert.equal(actionTag.value, 'Propose-Add-Controller-Notice');
-      assert.deepEqual(JSON.parse(replyMessage.Data), {
-        proposalNumber: 1,
-        yays: [],
-        nays: [],
-        proposalName: "Add-Controller_new-controller",
-        controller: 'new-controller',
-        type: "Add-Controller",
-      });
-
-      const proposals = await getProposals(result.Memory);
-      assert.deepEqual(proposals, {
-        "Add-Controller_new-controller": {
+        "Add-Controller_new-controller",
+        {
           proposalNumber: 1,
           yays: [],
           nays: [],
           controller: 'new-controller',
           type: "Add-Controller",
-        }
-      });
+        },
+        false,
+      );
     });
 
     it('should allow casting a "Yay" vote along with the proposal', async () => {
-      const result = await handle({
-        options: {
+      const resultMemory = await validProposalTest(
+        {
           Tags: [
             { name: 'Action', value: 'Propose-Add-Controller' },
             { name: 'Controller', value: 'new-controller' },
             { name: 'Vote', value: 'Yay' },
           ],
         },
-        mem: testMemory,
-      });
-      const replyMessage = result.Messages[0];
-      const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
-      assert.notEqual(actionTag, undefined, "Expected an action tag");
-      assert.equal(actionTag.value, 'Propose-Add-Controller-Notice');
-      assert.deepEqual(JSON.parse(replyMessage.Data), {
-        proposalNumber: 1,
-        yays: {
-          [PROCESS_OWNER]: true
-        },
-        nays: [],
-        proposalName: "Add-Controller_new-controller",
-        controller: 'new-controller',
-        type: "Add-Controller",
-      });
+        "Add-Controller_new-controller",
+        {
+          proposalNumber: 1,
+          yays: {
+            [PROCESS_OWNER]: true
+          },
+          nays: [],
+          controller: 'new-controller',
+          type: "Add-Controller",
+        }
+      );
 
       // The proposal should now be completed
-      assert.deepEqual(await getProposals(), {});
+      assert.deepEqual(await getProposals(resultMemory), {});
 
       // The controller should now be added
-      assert.deepEqual(await getControllers(result.Memory), [
+      assert.deepEqual(await getControllers(resultMemory), [
         PROCESS_OWNER,
         'new-controller',
       ]);
     });
 
     it('should allow casting a "Nay" vote along with the proposal', async () => {
-      const result = await handle({
-        options: {
+      const resultMemory = await validProposalTest(
+        {
           Tags: [
             { name: 'Action', value: 'Propose-Add-Controller' },
             { name: 'Controller', value: 'new-controller' },
-            { name: 'Vote', value: 'nay' },
+            { name: 'Vote', value: 'Nay' },
           ],
         },
-        mem: testMemory,
-      });
-      const replyMessage = result.Messages[0];
-      const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
-      assert.notEqual(actionTag, undefined, "Expected an action tag");
-      assert.equal(actionTag.value, 'Propose-Add-Controller-Notice');
-      assert.deepEqual(JSON.parse(replyMessage.Data), {
-        proposalNumber: 1,
-        yays: [],
-        nays: {
-          [PROCESS_OWNER]: true
-        },
-        proposalName: "Add-Controller_new-controller",
-        controller: 'new-controller',
-        type: "Add-Controller",
-      });
+        "Add-Controller_new-controller",
+        {
+          proposalNumber: 1,
+          yays: [],
+          nays: {
+            [PROCESS_OWNER]: true
+          },
+          controller: 'new-controller',
+          type: "Add-Controller",
+        }
+      );
 
       // The proposal should now be completed
-      const proposals = await getProposals(result.Memory);
+      const proposals = await getProposals(resultMemory);
       assert.deepEqual(proposals, {});
+
+      // The controller should note have been added
+      assert.deepEqual(await getControllers(resultMemory), [
+        PROCESS_OWNER,
+      ]);
     });
 
     it('should disallow creation of a duplicate proposal', async () => {
@@ -216,10 +189,8 @@ describe('AOS Handlers:', () => {
       const errorTag = replyMessage.Tags.find(tag => tag.name === 'Error');
       assert(errorTag.value.includes('Proposal already exists'));
     });
-  });
-
-  describe("Vote", () => {
-    describe("for a proposal to add a controller", () => {
+  
+    describe("Vote", () => {
       let testMemory;
       before(async () => {
         const result = await handle({
@@ -232,7 +203,7 @@ describe('AOS Handlers:', () => {
         });
         testMemory = result.Memory;
       });
-
+  
       it('should not allow submitting the vote from a non-controller', async () => {
         const result = await handle({
           options: {
@@ -255,7 +226,7 @@ describe('AOS Handlers:', () => {
         assert(errorTag.value.includes('Sender is not a registered Controller!'));
         assert(replyMessage.Data.includes('Sender is not a registered Controller!'));
       });
-
+  
       it('should not allow voting on a non-existent proposal', async () => {
         const result = await handle({
           options: {
@@ -276,7 +247,7 @@ describe('AOS Handlers:', () => {
         assert(errorTag.value.includes('Proposal does not exist'));
         assert(replyMessage.Data.includes('Proposal does not exist'));
       });
-
+  
       it("should allow voting yay on a proposal", async () => {
         const result = await handle({
           options: {
@@ -288,7 +259,7 @@ describe('AOS Handlers:', () => {
           },
           mem: testMemory,
         });
-
+  
         const replyMessage = result.Messages[0];
         const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
         assert.notEqual(actionTag, undefined, "Expected an action tag");
@@ -302,12 +273,12 @@ describe('AOS Handlers:', () => {
           controller: 'new-controller',
           type: "Add-Controller",
         });
-
+  
         // Ensure that the vote is now over
         const proposals = await getProposals(result.Memory);
         assert.deepEqual(proposals, {});
       });
-
+  
       it("should allow voting nay on a proposal", async () => {
         const result2 = await handle({
           options: {
@@ -319,7 +290,7 @@ describe('AOS Handlers:', () => {
           },
           mem: testMemory,
         });
-
+  
         const replyMessage = result2.Messages[0];
         const actionTag = replyMessage.Tags.find(tag => tag.name === 'Action');
         assert.notEqual(actionTag, undefined, "Expected an action tag");
@@ -333,12 +304,12 @@ describe('AOS Handlers:', () => {
           controller: 'new-controller',
           type: "Add-Controller",
         });
-
+  
         // Ensure that the vote is now over
         const proposals = await getProposals(result2.Memory);
         assert.deepEqual(proposals, {});
       });
-
+  
       describe("with multiple controllers", () => {
         before(async () => {
           const result = await handle({
@@ -365,7 +336,7 @@ describe('AOS Handlers:', () => {
           });
           testMemory = result2.Memory;
         });
-
+  
         it("should not complete the non-voted proposal with an initial yay vote", async () => {
           const result = await handle({
             options: {
@@ -391,7 +362,7 @@ describe('AOS Handlers:', () => {
           });
         });
       });
-
+  
       it("should complete the proposal with a quorum of yay votes", async () => {
         const result = await handle({
           options: {
@@ -425,16 +396,16 @@ describe('AOS Handlers:', () => {
           controller: 'new-controller2',
           type: "Add-Controller",
         });
-
+  
         // Ensure the proposal is now completed
         const proposals = await getProposals(result2.Memory);
         assert.deepEqual(proposals, {});
-
+  
         // Ensure that the new controller is now added
         const controllers = await getControllers(result2.Memory);
         assert.deepEqual(controllers, [PROCESS_OWNER, 'new-controller', 'new-controller2']);
       });
-
+  
       it("should end the proposal with a quorum of nay votes", async () => {
         const result = await handle({
           options: {
@@ -468,16 +439,16 @@ describe('AOS Handlers:', () => {
           controller: 'new-controller2',
           type: "Add-Controller",
         });
-
+  
         // Ensure the proposal is now completed
         const proposals = await getProposals(result2.Memory);
         assert.deepEqual(proposals, {});
-
+  
         // Ensure that the controller was NOT added
         const controllers = await getControllers(result2.Memory);
         assert.deepEqual(controllers, [PROCESS_OWNER, 'new-controller']);
       });
-
+  
       it("should end the proposal on a split vote with no majority", async () => {
         const result = await handle({
           options: {
@@ -512,16 +483,16 @@ describe('AOS Handlers:', () => {
           controller: 'new-controller2',
           type: "Add-Controller",
         });
-
+  
         // Ensure the proposal is now completed
         const proposals = await getProposals(result2.Memory);
         assert.deepEqual(proposals, {});
-
+  
         // Ensure that the controller was NOT added
         const controllers = await getControllers(result2.Memory);
         assert.deepEqual(controllers, [PROCESS_OWNER, 'new-controller']);
       });
-
+  
       it("should pass the proposal with 2 out of 3 yay votes", async () => {
         // After this, PROCESS_OWNER, 'new-controller', and 'new-controller3' should be controllers
         const { memory: rubberStampMemory } = await rubberStampProposal({
@@ -547,7 +518,7 @@ describe('AOS Handlers:', () => {
           },
           mem: rubberStampMemory,
         });
-
+  
         const result = await handle({
           options: {
             Tags: [
@@ -558,7 +529,7 @@ describe('AOS Handlers:', () => {
           },
           mem: firstVoteresult.Memory,
         });
-
+  
         // Ensure that 'new-controller2' is now added
         const controllers2 = await getControllers(result.Memory);
         assert.deepEqual(controllers2, [PROCESS_OWNER, 'new-controller', 'new-controller2', 'new-controller3']);
